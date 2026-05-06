fdf34d8653e255e0e268f7be6c8a0da5f6dfd54a
Deepu Pandey <pandeydeepu903@gmail.com>
2026-05-06 15:29:05 +0530

    connected frontend with live backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://taskmanager-api-sbcs.onrender.com",
 });
 
 // Interceptor to add JWT token to requests
